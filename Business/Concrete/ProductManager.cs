e61af0c6072c1b8284aaa90ba029ef9cb1cd038a
oguzcevik <oguzhan.cevik@net-bt.com.tr>
2022-08-10 17:33:29 +0300

    Result Added.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Business.Constant;
+using Core.DataAccess.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
@@ -17,11 +19,11 @@ namespace Business.Concrete
     {
       _productDal = productDal;
     }
-    public List<Product> GetAll()
+    public IDataResult<List<Product>> GetAll()
     {
-      return _productDal.GetAll();
+      return new DataResult(_productDal.GetAll());
     }
-
+     
     public List<Product> GetAllByCategoryId(int id)
     {
       return _productDal.GetAll(p => p.CategoryId == id);
@@ -32,9 +34,24 @@ namespace Business.Concrete
       return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
     }
 
+    public Product GetById(int productId)
+    {
+      return _productDal.Get(p => p.ProductId == productId);
+    }
+
     public List<ProductDetailDto> GetProductDetails()
     {
       return _productDal.GetProductDetails();
+    }
+
+    IResult IProductService.Add(Product product)
+    {
+      _productDal.Add(product);
+      if(product.ProductName.Length < 2)
+      {
+        return new ErrorResult(Messages.ProductNameInvalid);
+      }
+      return new SuccessResult(Messages.ProductAdded);
     }
   }
 }
---
+++ Core/DataAccess/Utilities/Results/DataResult.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DataAccess.Utilities.Results
+{
+  public class DataResult : IDataResult
+  {
+  }
+}
